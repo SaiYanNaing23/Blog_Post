9e7099c196e0bb1a7655c01f7b5d70aa9a96b85b
Saiyannaing23 <pesilay2372002@gmail.com>
2023-09-06 03:07:38 +0630

    blog Post Input

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BlogPost></BlogPost>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BlogPost from './components/BlogPost.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    BlogPost
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
